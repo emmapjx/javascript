3bc5fdaa2b313d7b567249843d8f7223d9f7e7ca
emmapjx <106750385+emmapjx@users.noreply.github.com>
2024-08-14 11:32:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var img =window.document.getElementById('imagem')
 var data = new Date()
 var hora data.getHours()
    msg.innerHTML = `Agora sao ${hora} horas.`
-   if (hora >=0 && hora < 12) {
+   if (hora >=0 && hora < 11) {
       img.src = 'manhã.jpg'
 
 } else if(hora >=11 && hora <18) {
